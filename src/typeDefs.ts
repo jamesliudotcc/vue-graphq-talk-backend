16165b619ea461274bea5c2ef960c803a320c262
jamesliudotcc <james@jamesliu.cc>
2019-05-16 14:14:47 -0700

    added types for House, Item, Store

--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -21,4 +21,20 @@ export const typeDefs = gql`
     token: String
     user: User
   }
+  type House {
+    name: String!
+    users: [User]
+    items: [Item]
+  }
+  type Item {
+    name: String!
+    qty: Int
+    done: Boolean
+    stores: [Store]
+    house: House!
+    purchasedBy: User
+  }
+  type Store {
+    name: String!
+  }
 `;
